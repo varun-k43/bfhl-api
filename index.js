f8903aa062f600016db7bd406ee24edcc0ea3aa1
varun-k43 <varunkambrath@gmail.com>
2026-04-24 18:49:38 +0530

    Fix PORT for deployment

--- index.js
+++ index.js
@@ -173,4 +173,5 @@ app.post("/bfhl", (req, res) => {
   res.json(response);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
